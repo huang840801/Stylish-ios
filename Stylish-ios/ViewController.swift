eaac71cfa447fe453bb191668759f4c4e5b84e01
GuanHongHuang <david840801@gmail.com>
2019-10-11 13:42:05 +0800

    first commit

---
+++ Stylish-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stylish-ios
+//
+//  Created by 黃瓘閎 on 2019/10/11.
+//  Copyright © 2019 黃瓘閎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stylish-iosTests/Stylish_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  Stylish_iosTests.swift
+//  Stylish-iosTests
+//
+//  Created by 黃瓘閎 on 2019/10/11.
+//  Copyright © 2019 黃瓘閎. All rights reserved.
+//
+
+import XCTest
+@testable import Stylish_ios
+
+class Stylish_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
