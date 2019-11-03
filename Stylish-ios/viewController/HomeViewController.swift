38575b195bee148e98412d1dd40dca2578776d01
GuanHongHuang <david840801@gmail.com>
2019-11-03 09:19:22 +0800

    Making api call

--- Stylish-ios/viewController/HomeViewController.swift
+++ Stylish-ios/viewController/HomeViewController.swift
@@ -25,6 +25,44 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.dataSource = self
         tableView.delegate = self
                 
+//        let url = URL(string: "https://api.appworks-school.tw/api/1.0/marketing/hots")!
+//
+//        var request = URLRequest(url: URL)
+//        request.httpMethod = "GET"
+//
+        
+        //create the url with NSURL
+         let url = URL(string: "https://api.appworks-school.tw/api/1.0/marketing/hots")! //change the url
+
+         //create the session object
+         let session = URLSession.shared
+
+         //now create the URLRequest object using the url object
+         let request = URLRequest(url: url)
+
+         //create dataTask using the session object to send data to the server
+         let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+
+             guard error == nil else {
+                 return
+             }
+
+             guard let data = data else {
+                 return
+             }
+
+            do {
+               //create json object from data
+               if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                  print(json)
+               }
+            } catch let error {
+              print(error.localizedDescription)
+            }
+         })
+
+         task.resume()
+        
     }
     
     // 必須實作的方法：每一組 section 有幾個 cell
